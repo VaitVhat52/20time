71d8ff6f7b4ef16c3bb9d62eb25383100ba438e7
VaitVhat52 <87101859+VaitVhat52@users.noreply.github.com>
2024-08-18 13:51:19 -0400

    Changed hero height

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="w-full max-h-[400px] bg-cover bg-no-repeat bg-[url('/herobg.jpg')] bg-center  flex flex-col justify-center items-center gap-3">
+    <div className="w-full max-h-[3500px] bg-cover bg-no-repeat bg-[url('/herobg.jpg')] bg-center flex flex-col justify-center items-center gap-3">
       <h1 className="text-[5rem] font-medium text-white">
         Say hello to Zacky,
       </h1>
