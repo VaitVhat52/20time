523be6d31b7f4a4959f549ca567ac26520e3f76b
VaitVhat52 <87101859+VaitVhat52@users.noreply.github.com>
2024-08-19 16:15:16 -0400

    Add card preview

--- src/app/blog/new/page.jsx
+++ src/app/blog/new/page.jsx
@@ -8,6 +8,15 @@ import { Button } from "@/components/ui/button";
 import supabase from "../../../../utils/client";
 import { useRouter } from "next/navigation";
 import { useToast } from "@/components/ui/use-toast";
+import BlogItem from "@/components/BlogItem";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const newPost = () => {
   const date = new Date();
@@ -57,68 +66,89 @@ const newPost = () => {
   }
 
   return (
-    <div className="mt-32 w-full">
-      <form className="flex flex-col gap-5 mx-[200px]" onSubmit={handleSubmit}>
-        <div className="grid w-full gap-1.5">
-          <Label htmlFor="title">Title</Label>
-          <Input
-            type="title"
-            id="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="Title"
-            className="w-full"
-          />
-        </div>
-        <div className="grid w-full gap-1.5">
-          <Label htmlFor="Body">Body</Label>
-          <Textarea
-            placeholder="Type your body here."
-            id="body"
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-          />
-        </div>
-        <div className="grid w-full gap-1.5">
-          <Label htmlFor="title">Image Link</Label>
-          <Input
-            type="title"
-            id="image"
-            value={imgLink}
-            onChange={(e) => setImgLink(e.target.value)}
-            placeholder="Image Link"
-            className="w-full"
-          />
-        </div>
-        <div className="grid w-full gap-1.5">
-          <Label htmlFor="link">Link</Label>
-          <Input
-            type="link"
-            id="link"
-            placeholder="Link"
-            className="w-full"
-            value={link}
-            onChange={(e) => setLink(e.target.value)}
-          />
-          <p className="text-sm text-gray-500">
-            The URL to this blog post will be: 20time.vercel.app/blog/{link}
-          </p>
-        </div>
-        <div className="grid w-full gap-1.5">
-          <Input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            className="w-full"
-          />
-          <p className={`text-sm text-red-700 ${error ? "block" : "hidden"}`}>
-            Password is incorrect.
-          </p>
-        </div>
-        <Button type="submit">Button</Button>
-      </form>
+    <div className="w-full flex flex-col justify-center items-center">
+      <div className="max-w-max min-w-[1200px]">
+        <form
+          className="flex flex-col gap-5 mx-[200px]"
+          onSubmit={handleSubmit}
+        >
+          <div className="grid w-full gap-1.5">
+            <Label htmlFor="title">Title</Label>
+            <Input
+              type="title"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Title"
+              className="w-full"
+            />
+          </div>
+          <div className="grid w-full gap-1.5">
+            <Label htmlFor="Body">Body</Label>
+            <Textarea
+              placeholder="Type your body here."
+              id="body"
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+            />
+          </div>
+          <div className="grid w-full gap-1.5">
+            <Label htmlFor="title">Image Link</Label>
+            <Input
+              type="title"
+              id="image"
+              value={imgLink}
+              onChange={(e) => setImgLink(e.target.value)}
+              placeholder="Image Link"
+              className="w-full"
+            />
+          </div>
+          <div className="grid w-full gap-1.5">
+            <Label htmlFor="link">Link</Label>
+            <Input
+              type="link"
+              id="link"
+              placeholder="Link"
+              className="w-full"
+              value={link}
+              onChange={(e) => setLink(e.target.value)}
+            />
+            <p className="text-sm text-gray-500">
+              The URL to this blog post will be: 20time.vercel.app/blog/{link}
+            </p>
+          </div>
+          <div className="grid w-full gap-1.5">
+            <Input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              className="w-full"
+            />
+            <p className={`text-sm text-red-700 ${error ? "block" : "hidden"}`}>
+              Password is incorrect.
+            </p>
+          </div>
+          <Button type="submit">Button</Button>
+        </form>
+      </div>
+      <div className="w-full flex justify-center mt-3 hover:underline">
+        <Dialog>
+          <DialogTrigger>View Card Preview</DialogTrigger>
+          <DialogContent>
+            <div className="w-full flex justify-center items-center">
+              <BlogItem
+                img={imgLink}
+                title={title}
+                date={currentDate}
+                content={body}
+                link={"new"}
+              />
+            </div>
+          </DialogContent>
+        </Dialog>
+      </div>
     </div>
   );
 };
