43c8b8ffbc8491dcc1bb3eabd83982fb99e98647
VaitVhat52 <87101859+VaitVhat52@users.noreply.github.com>
2024-09-28 00:04:22 -0400

    Update page.jsx

--- src/app/research/page.jsx
+++ src/app/research/page.jsx
@@ -27,25 +27,25 @@ const research = () => {
           <TabsTrigger value="4">Research Paper 4</TabsTrigger>
         </TabsList>
         <TabsContent value="1">
-          <h2 className="font-serif text-4xl pb-8">
+          <h2 className="font-serif text-4xl pb-8 text-center">
             What is an Arduino and how does it work?
           </h2>
           <Markdown className="font-serif prose indent-8">{research1}</Markdown>
         </TabsContent>
         <TabsContent value="2">
-          <h2 className="font-serif text-4xl pb-8">
+          <h2 className="font-serif text-4xl pb-8 text-center">
             Research Paper 2 Coming Soon!
           </h2>
           <Markdown className="font-serif prose indent-8">{research1}</Markdown>
         </TabsContent>
         <TabsContent value="3">
-          <h2 className="font-serif text-4xl pb-8">
+          <h2 className="font-serif text-4xl pb-8 text-center">
             Research Paper 3 Coming Soon!
           </h2>
           <Markdown className="font-serif prose indent-8">{research1}</Markdown>
         </TabsContent>
         <TabsContent value="4">
-          <h2 className="font-serif text-4xl pb-8">
+          <h2 className="font-serif text-4xl pb-8 text-center">
             Research Paper 4 Coming Soon!
           </h2>
           <Markdown className="font-serif prose indent-8">{research1}</Markdown>
