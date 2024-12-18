bed1143baafd6c73b1766196576c73e9fa0ca7c3
VaitVhat52 <87101859+VaitVhat52@users.noreply.github.com>
2024-12-17 19:33:01 -0500

    Update page.jsx

--- src/app/reflection/page.jsx
+++ src/app/reflection/page.jsx
@@ -1,11 +1,62 @@
-import React from "react";
+"use client";
 
-const reflection = () => {
+import React, { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+
+const page = () => {
+  // const [title, setTitle] = useState();
+  // const [body, setBody] = useState();
+  // const [dateData, setDateData] = useState();
+
+  // const day = dateData?.getDate();
+  // const month = dateData?.toLocaleString("default", { month: "long" });
+  // const year = dateData?.getFullYear();
+  // const date = month + " " + Number(day + 1) + ", " + year;
+
+  // const params = useParams().id;
+
+  // async function fetchBlogData() {
+  //   const { data, error } = await supabase
+  //     .from("blog")
+  //     .select()
+  //     .match({
+  //       link: `${params}`,
+  //     });
+  //   setTitle(data[0]?.title);
+  //   setBody(data[0]?.content);
+  //   setDateData(new Date(`${data[0]?.created_at}`));
+
+  //   error ? console.log(error) : null;
+  // }
+
+  // useEffect(() => {
+  //   fetchBlogData();
+  // }, []);
+
+  const body = `
+
+
+`;
+
   return (
-    <div className="flex justify-center items-center w-full h-screen text-[5rem]">
-      PAGE UNDER CONSTRUCTION!
+    <div className="flex flex-col mx-auto mb-14">
+      <div className="mx-8 md:mx-[200px]">
+        <p className="text-5xl font-semibold mt-10 md:mt-20 text-left">
+          Reflection
+        </p>
+        <Markdown
+          remarkPlugins={[remarkGfm]}
+          rehypePlugins={[rehypeRaw]}
+          className="prose min-w-fit max-w-max"
+        >
+          {body}
+        </Markdown>
+      </div>
     </div>
   );
 };
 
-export default reflection;
+export default page;
